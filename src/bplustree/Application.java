fc1cfebca6503d081aa33ccef62e4ee55d523033
Ryan Erdmann <ryanerdmann@hotmail.com>
2014-03-09 16:57:00 -0500

    removed sydney method

--- src/bplustree/Application.java
+++ src/bplustree/Application.java
@@ -9,7 +9,6 @@ import java.util.Scanner;
 public class Application {
     
     public static void main(String[] args) {
-        sydneyIsCool();
         BPlusTree bpt = new BPlusTree();
         
         bpt.createTree();
@@ -25,42 +24,4 @@ public class Application {
         System.out.println("Done.");
     }
     
-    public static void sydneyIsCool() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Hello!");
-        boolean quit = false;
-        while (!quit) {
-            System.out.print("Please enter your favorite color or 'exit' to quit:  ");
-            String favoriteColor = scanner.nextLine();
-            favoriteColor = favoriteColor.toLowerCase();
-            switch (favoriteColor) {
-                case "blue":
-                    System.out.println("Wow! You must really like the sky.");
-                    break;
-                case "red":
-                    System.out.println("You must be a vampire!");
-                    break;
-                case "purple":
-                    System.out.println("Yummy!");
-                    break;
-                case "orange":
-                    System.out.println("Good choice!");
-                    break;
-                case "yellow":
-                    System.out.println("You're just a little ray of sunshine!");
-                    break;
-                case "pink":
-                    System.out.println("You must be in lurv!");
-                    break;
-                case "green":
-                    System.out.println("Wow, isn't nature cool!");
-                    break;
-                case "exit":
-                    quit = true;
-                    break;
-                default:
-                    System.out.println("Try again! That color is too ugly to process.");
-            }
-        }
-    }
 }
